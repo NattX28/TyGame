d834063788d3ad74ece193984684891ffdb8d159
NattX28 <natawato28@gmail.com>
2025-02-01 23:50:07 +0700

    post card component structure

--- frontend/app/feed/page.tsx
+++ frontend/app/feed/page.tsx
@@ -5,11 +5,12 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import PostCard from "@/components/post/PostCard";
 
 const Feed = () => {
   return (
     <div className="flex flex-col h-full p-6">
-      <Card className="flex-grow">
+      {/* <Card className="flex-grow">
         <CardHeader>
           <CardTitle>Welcome, Joji</CardTitle>
         </CardHeader>
@@ -17,7 +18,12 @@ const Feed = () => {
         <CardContent>
           <p>this is a Tygame, welcome to game world !!</p>
         </CardContent>
-      </Card>
+      </Card> */}
+      <div className="bg-orange-400">
+        <h2 className="text-lg font-bold">Hello Johny Doe</h2>
+        <p>this is a Tygame, welcome to game world !!</p>
+      </div>
+      <PostCard />
     </div>
   );
 };
---
+++ frontend/components/post/PostCard.tsx
@@ -0,0 +1,43 @@
+import { Button } from "../ui/button";
+import Image from "next/image";
+import { MessageCircle } from "lucide-react";
+import { ThumbsUp } from "lucide-react";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "../ui/card";
+
+const PostCard = () => {
+  return (
+    <Card className="w-3/5 px-0 mx-0 bg-main text-main-color">
+      <CardHeader className="p-4">
+        <CardTitle>Joney Star</CardTitle>
+        <CardTitle className="text-sm">17 January 2025 16:45:31</CardTitle>
+      </CardHeader>
+      <CardContent className="p-0">
+        <div className="relative w-full aspect-[16/9]">
+          <Image
+            src="/media/images/valorant-post.jpeg"
+            alt="valorant post"
+            fill
+            className="object-cover rounded-md"
+          />
+        </div>
+      </CardContent>
+      <CardFooter className="p-2 flex gap-4">
+        <Button className="flex items-center gap-1">
+          <ThumbsUp className="w-5 h-5" />
+          <span>Like</span>
+        </Button>
+        <Button className="flex items-center gap-1">
+          <MessageCircle className="w-5 h-5" />
+          <span>comment</span>
+        </Button>
+      </CardFooter>
+    </Card>
+  );
+};
+export default PostCard;
