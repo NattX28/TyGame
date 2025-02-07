93d735e99facff96ea6f487a85fdbbb233f565c5
stamp070 <vorathon007@gmail.com>
2025-02-07 21:21:17 +0700

    add page

---
+++ frontend/app/login/layout.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Loginpagelayout = ({
+    children,
+  }: {
+    children: React.ReactNode;
+  }) => {
+  return (
+    <div>layout</div>
+  )
+}
+
+export default Loginpagelayout
