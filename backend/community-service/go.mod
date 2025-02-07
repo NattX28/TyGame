4387d3938c8b6ef6a1cd53b26132b6ff1b367ce3
thelastcat15 <smart000firstfenix@gmail.com>
2025-02-07 18:43:10 +0700

    start create branch community-service

---
+++ backend/community-service/go.mod
@@ -0,0 +1,3 @@
+module community-service
+
+go 1.23.4
---
+++ backend/community-service/routes/post/edit.go
@@ -0,0 +1,51 @@
+package post
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/microcosm-cc/bluemonday"
+	"github.com/google/uuid"
+
+	"post-service/db"
+	"post-service/models"
+)
+
+func EditPostHandler(c *fiber.Ctx) error {
+	postID, err := uuid.Parse(c.Params("PostID"))
+	if err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "Invalid post ID"})
+	}
+
+	// Unpack Body To EditPost Form
+	var editPostReq models.EditPostRequest
+	if err := c.BodyParser(&editPostReq); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "Invalid request body"})
+	}
+
+	// Pull Data
+	var postReq models.Post
+	if err := db.DB.First(&postReq, postID).Error; err != nil {
+		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{"error": "Post not found"})
+	}
+
+	// Check Auth
+	userID, ok := c.Locals("UserID").(uuid.UUID)
+	if !ok {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "Invalid UserID"})
+	}
+	if (postReq.UserID != userID) {
+		return c.Status(fiber.StatusForbidden).JSON(fiber.Map{"error": "No Authorization"})
+	}
+
+	// Replace Data
+	policy := bluemonday.StrictPolicy()
+	safeContent := policy.Sanitize(editPostReq.Content)
+	postReq.Content = safeContent
+	postReq.Visibility = editPostReq.Visibility
+
+	// Store Data
+	if err := db.DB.Save(&postReq).Error; err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": "Failed to update post"})
+	}
+
+	return c.Status(fiber.StatusOK).JSON(fiber.Map{"message": "Post edited successfully"})
+}
