3ed3a0f7e1c5bd28c1df7c61cedf86ae978dc19b
thelastcat15 <smart000firstfenix@gmail.com>
2025-02-10 22:03:19 +0700

    create folder

---
+++ frontend/app/chat/page.tsx
@@ -0,0 +1,12 @@
+// import LoginForm from "./components/LoginForm";
+
+
+const page = () => {
+  return (
+    <div className="flex flex-col justify-center items-center">
+      <h2 className="text-3xl font-bold text-second-color">Login</h2>
+    </div>
+  );
+};
+
+export default page;
