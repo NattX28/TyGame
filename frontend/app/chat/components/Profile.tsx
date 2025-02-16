3fc4c6b8d890a3a9b96d8e8ddc888e01265de081
thelastcat15 <smart000firstfenix@gmail.com>
2025-02-16 23:12:54 +0700

    check 1

--- frontend/app/chat/components/Profile.tsx
+++ frontend/app/chat/components/Profile.tsx
@@ -1,25 +1,26 @@
-import { Avatar, AvatarFallback, AvatarImage } from "@radix-ui/react-avatar";
+import { Person } from "@/types/person";
+import { Avatar, AvatarImage } from "@radix-ui/react-avatar";
 
 const Profile = ({
-    friend,
+    person,
 }: {
-    friend: { id: number; name: string; avatar: string };
+    person: Person;
 }) => {
   return (
-    <div className="flex items-center min-w-44 h-12 border-white border-2 rounded-3xl px-2 relative cursor:pointer hover:bg-second transition">
-        <div className="h-8 w-8 relative">
-            <Avatar className="h-8 w-8 ">
-                <AvatarImage
-                    src="https://github.com/shadcn.png"
-                    alt="@shadcn"
-                    className="rounded-full"
-                />
-                <AvatarFallback className="rounded-lg">CN</AvatarFallback>
-            </Avatar>
+    <div className="py-2 flex items-center hover:bg-second transition">
+        <div className="h-14 w-14 relative">
+            <img
+                src={person.avatar}
+                alt={person.name}
+                className="rounded-full"
+            />
             {/* status online */}
             <div className="absolute bottom-0 right-0 w-3 h-3 bg-green-500 border-2 border-third rounded-full"></div>
         </div>
-        <p className="mx-2">{friend.name}</p>
+        <div className="ml-2">
+            <p className="mx-2">{person.name}</p>
+            <p className="mx-2">{person.name}</p>
+        </div>
     </div>
   );
 };
--- frontend/app/chat/page.tsx
+++ frontend/app/chat/page.tsx
@@ -1,24 +1,41 @@
-// import LoginForm from "./components/LoginForm";
+import Profile from "./components/Profile";
 // import Scrollbar from "./components/FriendList";
 import Search from "./components/Search";
 
+const FriendList = () => {
+  return (
+    <>
+      <Profile person={{name: "John Doe", avatar: "/media/images/first.jpg" }} />
+      <Profile person={{name: "John Doe", avatar: "/media/images/first.jpg" }} />
+      <Profile person={{name: "John Doe", avatar: "/media/images/first.jpg" }} />
+      <Profile person={{name: "John Doe", avatar: "/media/images/first.jpg" }} />
+      <Profile person={{name: "John Doe", avatar: "/media/images/first.jpg" }} />
+      <Profile person={{name: "John Doe", avatar: "/media/images/first.jpg" }} />
+      <Profile person={{name: "John Doe", avatar: "/media/images/first.jpg" }} />
+      <Profile person={{name: "John Doe", avatar: "/media/images/first.jpg" }} />
+      <Profile person={{name: "John Doe", avatar: "/media/images/first.jpg" }} />
+      <Profile person={{name: "John Doe", avatar: "/media/images/first.jpg" }} />
+      <Profile person={{name: "John Doe", avatar: "/media/images/first.jpg" }} />
+      <Profile person={{name: "John Doe", avatar: "/media/images/first.jpg" }} />
+      <Profile person={{name: "John Doe", avatar: "/media/images/first.jpg" }} />
+      <Profile person={{name: "John Doe", avatar: "/media/images/first.jpg" }} />
+    </>
+  )
+}
+
 const page = () => {
   return (
-    <div className="p-4 h-screen bg-frame flex flex-row gap-4">
-      <div className="bg-sub-frame h-full w-full max-md:hidden max-w-sm flex flex-col gap-4 px-4 py-4 ">
+    <div className="h-screen flex flex-row bg-black">
+      <div className="border-r border-second h-full w-full max-md:hidden max-w-sm flex flex-col gap-4 p-4">
         <div className="w-full mx-2">
           <p className="text-2xl font-bold tracking-widest">CHAT</p> 
         </div>
         <Search pholder={"Search chat"} />
         <div className="h-full w-full overflow-y-auto rounded-lg p-4 scrollbar-transparent">
-          {
-            <div>
-              
-            </div>
-          }
+          <FriendList />
         </div>
       </div>
-      <div className="bg-sub-frame h-full w-full">
+      <div className="h-full w-full">
       </div>
       {/* <div className="bg-sub-frame h-full"></div> */}
     </div>
