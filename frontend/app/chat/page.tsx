491497e72a0c7660829bdc0709726ff9851f3982
thelastcat15 <smart000firstfenix@gmail.com>
2025-02-20 17:26:52 +0700

    full success chat page with responsive

--- frontend/app/chat/page.tsx
+++ frontend/app/chat/page.tsx
@@ -1,8 +1,9 @@
 "use client"
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import { Person } from "@/types/person";
 import Profile from "./components/Profile";
 import Search from "./components/Search";
+import { MsgMe, MsgYou } from "./components/Message";
 import InputMessage from "./components/InputMessage";
 
 const FriendList = () => {
@@ -26,9 +27,19 @@ const FriendList = () => {
   )
 }
 
-const page = () => {
-  const chatRef = useRef<HTMLDivElement>(null);
+const ChatList = () => {
+  return (
+    <>
+      {/* <MsgMe msg={"Hello World 123456789  dwadawdad"} />
+      <MsgMe msg={"Hello World <h1>adwa</h1>"} />
+      
+      <MsgYou msg={"Hi there!"} person={{ name: "Jane Doe", avatar: "/media/images/first.jpg" }} /> */}
+      <MsgYou msg={"Hi thereaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa!"} person={{ name: "Jane Doe", avatar: "/media/images/first.jpg" }} />
+    </>
+  )
+}
 
+const page = () => {
   const [person, setPerson] = useState<Person | null>(null);
 
   useEffect(() => {
@@ -39,8 +50,8 @@ const page = () => {
   }, []);
 
   return (
-    <div className="h-screen flex flex-row bg-black">
-      <div className="border-r border-second h-full w-full max-md:hidden max-w-sm flex flex-col gap-4 py-4">
+    <div className="h-screen w-screen flex flex-row bg-black">
+      <div className="h-full hidden md:flex md:flex-col gap-4 py-4 border-r border-second">
         <div className="px-8 pt-4">
           <p className="text-2xl font-bold tracking-widest">CHAT</p> 
         </div>
@@ -49,10 +60,9 @@ const page = () => {
           <FriendList />
         </div>
       </div>
-      <div className="w-full h-screen">
         {person ? (
-          <div className="flex flex-col h-full">
-            <div className="w-full h-20 p-4 border-b border-second flex flex-row items-center">
+          <div className="flex-1 flex flex-col">
+            <div className="h-20 p-4 border-b border-second flex flex-row items-center">
               <img
                 src={person.avatar}
                 alt={person.name}
@@ -60,19 +70,17 @@ const page = () => {
               />
               <p className="pl-4">{person.name}</p>
               <div className="ml-auto">
-                <svg aria-label="Conversation information"  fill="currentColor" height="24" role="img" viewBox="0 0 24 24" width="24"><title>Conversation information</title><circle cx="12.001" cy="12.005" fill="none" r="10.5" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"></circle><circle cx="11.819" cy="7.709" r="1.25"></circle><line fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" x1="10.569" x2="13.432" y1="16.777" y2="16.777"></line><polyline fill="none" points="10.569 11.05 12 11.05 12 16.777" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"></polyline></svg>
+                <svg aria-label="Conversation information" fill="currentColor" height="24" role="img" viewBox="0 0 24 24" width="24">
+                  <circle cx="12.001" cy="12.005" fill="none" r="10.5" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"></circle>
+                  <circle cx="11.819" cy="7.709" r="1.25"></circle>
+                  <line fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" x1="10.569" x2="13.432" y1="16.777" y2="16.777"></line>
+                  <polyline fill="none" points="10.569 11.05 12 11.05 12 16.777" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"></polyline>
+                </svg>
               </div>
             </div>
-            <div className="flex-1 flex flex-col"> {/* Modified this div */}
-              <div className="flex-1 overflow-hidden">
-                <div
-                  ref={chatRef}
-                  className="h-full flex flex-col-reverse overflow-y-auto p-2 space-y-2-reverse scroll-smooth"
-                >
-                  <p>
-                    awdadawdaw
-                  </p>
-                </div>
+            <div className="flex-1 flex flex-col">
+              <div className="flex-1 flex flex-col-reverse max-w-full overflow-y-auto p-2 scroll-smooth whitespace-normal">
+                <ChatList />
               </div>
               <InputMessage pholder={"Type a message"} />
             </div>
@@ -80,7 +88,6 @@ const page = () => {
         ) : (
           <h1>Select Chat</h1>
         )}
-      </div>
     </div>
   );
 };
