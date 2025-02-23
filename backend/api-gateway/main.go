d639c722ceed066893e0b2c4a67541e81baba099
thelastcat15 <smart000firstfenix@gmail.com>
2025-02-23 23:00:01 +0700

    edit cors

--- backend/api-gateway/main.go
+++ backend/api-gateway/main.go
@@ -15,7 +15,7 @@ func main() {
 
 	// CORS middleware
     app.Use(cors.New(cors.Config{
-        AllowOrigins: "http://localhost:3000", 
+        AllowOrigins: "*", 
         AllowHeaders: "Origin, Content-Type, Accept, Authorization",
         AllowMethods: "GET,POST,PUT,DELETE",
     }))
