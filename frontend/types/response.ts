fa2346299682dcaaedc4ddc6d02ae556d0df53c9
TheLastCat <84333919+thelastcat15@users.noreply.github.com>
2025-02-25 20:05:00 +0000

    success post service

--- frontend/types/response.ts
+++ frontend/types/response.ts
@@ -1,5 +1,9 @@
 import { Comment } from "./types";
 
+export interface getMessageResponse {
+  message: string;
+}
+
 export interface getCommentsResponse {
   message: string;
   comments: Comment[];
