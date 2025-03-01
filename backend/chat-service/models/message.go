cdf30b349ed507545dca399234ecf74ea07ba89b
TheLastCat <84333919+thelastcat15@users.noreply.github.com>
2025-03-01 23:25:21 +0000

    add json to model

--- backend/chat-service/models/message.go
+++ backend/chat-service/models/message.go
@@ -5,9 +5,9 @@ import (
 )
 
 type Message struct {
-	ID       	uint      	`gorm:"primaryKey"`
-	RoomID   	uuid.UUID 	`gorm:"index:idx_room_timestamp,priority:1"`  // Composite index part 1
-	SenderID   	uuid.UUID
-	Content  	string    	`gorm:"type:text;charset=utf8mb4"`
-	Timestamp	int64      	`gorm:"index:idx_room_timestamp,priority:2"`  // Composite index part 2
+    ID        uint       `json:"id" gorm:"primaryKey"`
+    RoomID    uuid.UUID  `json:"room_id" gorm:"index:idx_room_timestamp,priority:1"`
+    SenderID  uuid.UUID  `json:"sender_id"`
+    Content   string     `json:"content" gorm:"type:text;charset=utf8mb4"`
+    Timestamp int64      `json:"timestamp" gorm:"index:idx_room_timestamp,priority:2"`
 }
