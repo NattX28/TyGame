6598e363a53a24bd261327bfd141d1b726ca4af6
NattX28 <natawato28@gmail.com>
2025-03-02 23:42:44 +0700

    delete 8080

--- frontend/services/api.ts
+++ frontend/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 // instance of axios
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:8080", // URL backend
+  baseURL: process.env.NEXT_PUBLIC_API_URL, // URL backend
   withCredentials: true, // cookie
 });
 
