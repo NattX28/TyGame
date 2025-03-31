c58e48234b238bbcdddefbd391ed1d8baf165b0b
thelastcat15 <smart000firstfenix@gmail.com>
2025-03-31 08:57:54 +0700

    .

--- frontend/services/api.ts
+++ frontend/services/api.ts
@@ -1,12 +1,9 @@
 import axios from "axios";
 
-// const deployEnv = process.env.NEXT_DEPLOY || "local";
-// const Endpoint_Gateway = deployEnv === "prod" ? process.env.NEXT_PUBLIC_API_URL : "http://localhost:8080";
-// const Endpoint_Gateway_ws = deployEnv === "prod" ? process.env.NEXT_PUBLIC_WS_URL : "ws://localhost:8080";
+const prod = process.env.NODE_ENV === 'production'
+const Endpoint_Gateway = prod ? "https://tygame.up.railway.app" : "http://localhost:8080";
+const Endpoint_Gateway_ws = prod ? "ws://tygame.up.railway.app" : "ws://localhost:8080";
 
-const deployEnv = process.env.NEXT_DEPLOY || "local";
-const Endpoint_Gateway = "https://tygame.up.railway.app";
-const Endpoint_Gateway_ws = "ws://tygame.up.railway.app";
 
 const api = axios.create({
   baseURL: Endpoint_Gateway, // URL for backend
